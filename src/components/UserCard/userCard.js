c40ed8f79d6fe6c6ec1ba67a18fd592da2f8f645
Anita <raskovanita1@gmail.com>
2021-04-27 18:58:42 +0200

    css changes for user card

--- src/components/UserCard/userCard.js
+++ src/components/UserCard/userCard.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './userCardStyles.css';
+import TextTruncate from 'react-text-truncate';
 
 
 class UserCard extends Component {
 
   render() {
     return (
-      <Link to="/repos">
-        <button className="wrapper">
+      <Link to="/repos" className="user-link">
+        <button className="user-wrapper">
           <img src={this.props.avatar} alt="avatar" className="user-avatar"/>
           <div className="user-info">
-            <h5 className="user-name">{this.props.username}</h5>
-            <div className="user-description">{this.props.description}</div>
+            <h4 className="user-name">{this.props.username}</h4>
+            <div className="user-description">
+              <TextTruncate
+                  line={2}
+                  element="div"
+                  truncateText="..."
+                  text={this.props.description}
+                />
+            </div>
           </div>
         </button>
       </Link>
